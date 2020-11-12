1f8030b03c1b43872552caa5fd72c74647cbe008
BdeBarrio <gael12345@>
2020-11-12 12:29:37 +0100

    1

--- src/Proyecte.java
+++ src/Proyecte.java
@@ -1,5 +1,46 @@
+import java.util.Scanner;
+
 public class Proyecte {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int opcio;
+
+        System.out.println("################################");
+        System.out.println("Benvinguts al forum de BdeBarrio");
+        System.out.println("################################");
+        System.out.println();
+
+        boolean opcions= true;
+        while(opcions){
+            System.out.println("Que vols fer?\n" +
+                    "1. Registrar-te\n" +
+                    "2. Iniciar sessió\n" +
+                    "3. Sortir");
+            opcio = sc.nextInt();
+            sc.nextLine();
+
+            switch (opcio){
+                case 1:
+                    System.out.println("Indica el nom de usuari");
+
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    System.out.println("Has sortit del programa.");
+                    opcions=false;
+                    break;
+                default:
+                    System.out.println("Aquesta opció no es valida torna a indicar-la.");
+                    break;
+            }
+
+        }
+
+
+
+
+
 
     }
 }
